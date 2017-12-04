0e7f303c5e709db6400dd3db25acce30dcf5af73
lecault <loic.ecault@bretagne.bzh>
2017-12-04 14:36:16 +0100

    maj fiches territoires

---
+++ territoire/customlayers/fiche_dep.js
@@ -0,0 +1,19 @@
+{
+mviewer.customLayers.fiche_dep = {};
+mviewer.customLayers.fiche_dep.layer = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            url: "apps/region/territoire/data/departement_simple.geojson",
+            format: new ol.format.GeoJSON()
+        }),
+style: new ol.style.Style({
+          stroke: new ol.style.Stroke({
+            color: 'rgba(114,72,127,255)',
+            width: 2
+          }),
+          fill: new ol.style.Fill({
+            color: 'rgba(114,72,127,0.2)'
+          })
+})        
+  });
+mviewer.customLayers.fiche_dep.handle = false;
+} 
