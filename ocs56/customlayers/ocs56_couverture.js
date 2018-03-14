58288926414b71d85e4085e049e7a2bd8276336e
spelhate <seb.pelhate@gmail.com>
2018-03-14 17:06:16 +0100

    OCS tuiles vecteur

---
+++ ocs56/customlayers/ocs56_couverture.js
@@ -0,0 +1,96 @@
+{
+mviewer.customLayers.ocs56_couverture = {};
+mviewer.customLayers.ocs56_couverture.classification_couverture = {};
+mviewer.customLayers.ocs56_couverture.classification_couverture.a = {
+        "CS1%":{"id":1,"title":"Sans végétation","field":"couverture","value":"CS1%","fill":"#fd6270","stroke":"#fd6270",  "strokewidth":0,"maxscaledenominator":"noscale"},
+        "CS2%":{"id":2,"title":"Avec végétation","field":"couverture","value":"CS2%","fill":"#96de41","stroke":"#96de41",  "strokewidth":0,"maxscaledenominator":"noscale"}
+  };
+mviewer.customLayers.ocs56_couverture.classification_couverture.b = {
+        "CS1?1%":{"id":3,"title":"Surfaces anthropisées","field":"couverture","value":"CS1?1%","fill":"#fd6270","stroke":"#fd6270",  "strokewidth":0,"maxscaledenominator":"100000"},
+        "CS1?2%":{"id":4,"title":"Surfaces naturelles","field":"couverture","value":"CS1?2%","fill":"#cccccc","stroke":"#cccccc",  "strokewidth":0,"maxscaledenominator":"100000"},
+        "CS2?1%":{"id":5,"title":"Végétation ligneuse","field":"couverture","value":"CS2?1%","fill":"#96de41","stroke":"#96de41",  "strokewidth":0,"maxscaledenominator":"100000"},
+        "CS2?2%":{"id":6,"title":"Végétation non ligneuse","field":"couverture","value":"CS2?2%","fill":"#ccf24d","stroke":"#ccf24d",  "strokewidth":0,"maxscaledenominator":"100000"}
+  };
+mviewer.customLayers.ocs56_couverture.classification_couverture.c = {
+        "CS1?1?1%":{"id":7,"title":"Zones imperméables","field":"couverture","value":"CS1?1?1%","fill":"#fd6270","stroke":"#fd6270",  "strokewidth":0,"maxscaledenominator":"50000"},
+        "CS1?1?2%":{"id":8,"title":"Zones perméables","field":"couverture","value":"CS1?1?2%","fill":"#ffff99","stroke":"#ffff99",  "strokewidth":0,"maxscaledenominator":"50000"},
+        "CS1?2?1%":{"id":9,"title":"Sols nus","field":"couverture","value":"CS1?2?1%","fill":"#cccccc","stroke":"#cccccc",  "strokewidth":0,"maxscaledenominator":"50000"},
+        "CS1?2?2%":{"id":10,"title":"Surfaces d'eau","field":"couverture","value":"CS1?2?2%","fill":"#00ccf2","stroke":"#00ccf2",  "strokewidth":0,"maxscaledenominator":"50000"},
+        "CS2?1?1%":{"id":11,"title":"Formations arborées","field":"couverture","value":"CS2?1?1%","fill":"#96de41","stroke":"#96de41",  "strokewidth":0,"maxscaledenominator":"50000"},
+        "CS2?1?2%":{"id":12,"title":"Formations arbustives et sous-arbrisseaux","field":"couverture","value":"CS2?1?2%","fill":"#ad6659","stroke":"#ad6659",  "strokewidth":0,"maxscaledenominator":"50000"},
+        "CS2?1?3%":{"id":13,"title":"Autres formations ligneuses","field":"couverture","value":"CS2?1?3%","fill":"#e68000","stroke":"#e68000",  "strokewidth":0,"maxscaledenominator":"50000"},
+        "CS2?2?1%":{"id":14,"title":"Formations herbacées","field":"couverture","value":"CS2?2?1%","fill":"#a6ff8a","stroke":"#a6ff8a",  "strokewidth":0,"maxscaledenominator":"50000"}
+    };
+mviewer.customLayers.ocs56_couverture.classification_couverture.d = {
+      "CS1.1.1.1":{"id":15,"title":"Zones bâties","field":"couverture","value":"CS1.1.1.1","fill":"#fd6270","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS1.1.1.2":{"id":16,"title":"Zones non bâties","field":"couverture","value":"CS1.1.1.2","fill":"#f4afa7","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS1.1.2.1":{"id":17,"title":"Zones à matériaux minéraux- pierre-terre","field":"couverture","value":"CS1.1.2.1","fill":"#dad6bf","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS1.1.2.2":{"id":18,"title":"Zones à autres matériaux composites","field":"couverture","value":"CS1.1.2.2","fill":"#b6b3ff","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS1.2.1.0":{"id":19,"title":"Autres sols nus","field":"couverture","value":"CS1.2.1.0","fill":"#dad6d2","stroke":"#000000#000000#6e6e6e#000000","maxscaledenominator":"10000"},
+      "CS1.2.1.1":{"id":20,"title":"Sable et limons","field":"couverture","value":"CS1.2.1.1","fill":"#f4e8ba","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS1.2.1.2":{"id":21,"title":"Pierres","field":"couverture","value":"CS1.2.1.2","fill":"#b0968a","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS1.2.1.3":{"id":22,"title":"Rochers","field":"couverture","value":"CS1.2.1.3","fill":"#96966e","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS1.2.2.1":{"id":23,"title":"Eaux continentales","field":"couverture","value":"CS1.2.2.1","fill":"#7dc5ff","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS1.2.2.2":{"id":24,"title":"Eaux maritimes","field":"couverture","value":"CS1.2.2.2","fill":"#009cd6","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS1.2.3.1":{"id":25,"title":"Névés","field":"couverture","value":"CS1.2.3.1","fill":"#beffe8","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS1.2.3.2":{"id":26,"title":"Glaciers","field":"couverture","value":"CS1.2.3.2","fill":"#dbf2f7","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS2.1.1.1":{"id":27,"title":"Peuplements de feuillus","field":"couverture","value":"CS2.1.1.1","fill":"#96de41","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS2.1.1.2":{"id":28,"title":"Peuplements de conifères","field":"couverture","value":"CS2.1.1.2","fill":"#5c7326","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS2.1.1.3":{"id":29,"title":"Peuplements mixtes","field":"couverture","value":"CS2.1.1.3","fill":"#7dbd4a","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS2.1.2.1":{"id":30,"title":"Landes","field":"couverture","value":"CS2.1.2.1","fill":"#a8a828","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS2.1.2.2":{"id":31,"title":"Végétation sclérophylle","field":"couverture","value":"CS2.1.2.2","fill":"#adba8a","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS2.1.2.3":{"id":32,"title":"Formations arbustives basses linéaires ou organisées","field":"couverture","value":"CS2.1.2.3","fill":"#ad6659","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS2.1.2.5":{"id":33,"title":"Fourrés","field":"couverture","value":"CS2.1.2.5","fill":"#adad59","stroke":"#000000#6e6e6e#000000","maxscaledenominator":"10000"},
+      "CS2.1.3.1":{"id":34,"title":"Autres formations arbustives","field":"couverture","value":"CS2.1.3.1","fill":"#aa66cd","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS2.2.1.1":{"id":35,"title":"Prairies","field":"couverture","value":"CS2.2.1.1","fill":"#a6ff8a","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS2.2.1.2":{"id":36,"title":"Pelouses","field":"couverture","value":"CS2.2.1.2","fill":"#d7ffb5","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS2.2.1.3":{"id":37,"title":"Jardins","field":"couverture","value":"CS2.2.1.3","fill":"#4fff67","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS2.2.1.4":{"id":38,"title":"Terres arables","field":"couverture","value":"CS2.2.1.4","fill":"#fff07a","stroke":"#000000", "strokewidth":1,"maxscaledenominator":"10000"},
+      "CS2.2.1.5":{"id":39,"title":"Autres formations herbacées","field":"couverture","value":"CS2.2.1.5","fill":"#fff07a","stroke":"#000000#6e6e6e#000000","maxscaledenominator":"10000"}
+  };
+
+
+mviewer.customLayers.ocs56_couverture.style1 = function (feature, resolution) {
+    var level = "";
+    var ref = "";
+    var val = feature.get("couverture");
+    if (resolution > 76) {
+            level = "a";
+            ref = val.substring(0,3)+"%";
+    } else if (resolution > 38) {
+            level = "b";
+            ref = val.substr(0,3)+"?"+val.substr(4,1)+"%"
+    } else if (resolution > 9) {
+            level = "c";
+            ref = val.substr(0,3)+"?"+val.substr(4,1)+"?"+val.substr(6,1)+"%"
+    } else if (resolution > 2) {
+            level = "d";
+            ref = val;
+    }
+
+    feature.getProperties()["label"] = mviewer.customLayers.ocs56_couverture.classification_couverture[level][ref].title;
+    var style = new ol.style.Style({
+    fill: new ol.style.Fill({
+      color: mviewer.customLayers.ocs56_couverture.classification_couverture[level][ref].fill
+    }),
+    stroke: new ol.style.Stroke({
+      color: mviewer.customLayers.ocs56_couverture.classification_couverture[level][ref].fill,
+      width: 0
+    })
+  });
+    return style;
+};
+
+mviewer.customLayers.ocs56_couverture.layer = new ol.layer.VectorTile({
+      style:  mviewer.customLayers.ocs56_couverture.style1,
+      minResolution: 2,
+      maxResolution: 306,
+      source: new ol.source.VectorTile({
+        tilePixelRatio: 1, // oversampling when > 1
+        tileGrid: ol.tilegrid.createXYZ({maxZoom: 20}),
+        format: new ol.format.MVT(),
+        url: 'https://ows.region-bretagne.fr/geoserver/gwc/service/tms/1.0.0/rb:ocs56_origine@EPSG%3A3857@pbf/{z}/{x}/{-y}.pbf'
+      })
+});
+mviewer.customLayers.ocs56_couverture.handle = false;
+}
---
+++ ocs56/customlayers/ocs56_usage.js
@@ -0,0 +1,127 @@
+{
+mviewer.customLayers.ocs56_usage = {};
+mviewer.customLayers.ocs56_usage.classification_usage = {};
+mviewer.customLayers.ocs56_usage.classification_usage.a = {
+        "US1%":{"title":"Production primaire","fill":"#ffffa8","stroke":"#fd6270"},
+        "US2%":{"title":"Production secondaire, tertiaire et usage résidentiel","fill":"#cc0000"},
+        "US3%":{"title":"Production secondaire, tertiaire et usage résidentiel","fill":"#cc0000"},
+        "US4%":{"title":"Autre usage","fill":"#e6004d"},
+        "US5%":{"title":"Production secondaire, tertiaire et usage résidentiel","fill":"#e6004d"},
+        "US6%":{"title":"Autre usage","fill":"#ff4dff"}
+};
+mviewer.customLayers.ocs56_usage.classification_usage.b = {
+        "US1?1%":{"title":"Agriculture","fill":"#ffffa8"},
+        "US1?2%":{"title":"Sylviculture","fill":"#008000"},
+        "US1?3%":{"title":"Activités d'extraction","fill":"#78de41"},
+        "US1?4%":{"title":"Pêche et aquaculture","fill":"#000099"},
+        "US1?5%":{"title":"Autre","fill":"#e0967e"},
+        "US2.123":{"title":"Industrie","fill":"#a42424"},
+        "US2?4%":{"title":"Production d'énergie","fill":"#e04f3c"},
+        "US2?5%":{"title":"Autres Industries","fill":"#ffcc00"},
+        "US3?1%":{"title":"Commerce","fill":"#b18acc"},
+        "US3?2%":{"title":"Finance et information/communication","fill":"#894465"},
+        "US3?4%":{"title":"Loisirs, culturels…","fill":"#ffbee8"},
+        "US3?5%":{"title":"Autres services","fill":"#cd6699"},
+        "US4?1%":{"title":"Réseaux de transport","fill":"#cc0000"},
+        "US4?2%":{"title":"Services logistiques et de stockage","fill":"#5a5e6b"},
+        "US4?3%":{"title":"Réseaux d'utilité publique","fill":"#83a697"},
+        "US5?1%":{"title":"Usage résidentiel permanent","fill":"#ff0000"},
+        "US5?2%":{"title":"Usage résidentiel mixé à d'autres usages compatibles","fill":"#ff0000"},
+        "US5?3%":{"title":"Autre usage résidentiel","fill":"#fd6270"},
+        "US6?1%":{"title":"Zones de transition (ou de construction)","fill":"#e9d6d2"},
+        "US6?2%":{"title":"Zone abandonnée (friche)","fill":"#842e1b"},
+        "US6?3%":{"title":"Sans usages","fill":"#a98c78"},
+        "US6?4%":{"title":"Inconnu","fill":"#503737"}
+};
+mviewer.customLayers.ocs56_usage.classification_usage.c = {
+        "US1.1.1.1":{"title":"Pâturage","fill":"#a6ff8a"},
+        "US1.1.1.2":{"title":"Elevage","fill":"#a6ff8a"},
+        "US1.1.1.3":{"title":"Culture","fill":"#a6ff8a",},
+        "US1.1.2.1":{"title":"Stockage","fill":"#ffff99"},
+        "US1.1.2.2":{"title":"Elevage","fill":"#ffff99"},
+        "US1.1.2.3":{"title":"Autres","fill":"#ffff99"},
+        "US1.2":{"title":"Sylviculture","fill":"#008000"},
+        "US1.3":{"title":"Activités d'extraction","fill":"#78de41"},
+        "US1.5":{"title":"Autre","fill":"#e0967e"},
+        "US1.1.3":{"title":"Autoconsommation","fill":"#dad6bf"},
+        "US1.4.1":{"title":"Aquaculture","fill":"#7dc5ff"},
+        "US1.4.2":{"title":"Pêcherie","fill":"#00ccf2"},
+        "US2.123":{"title":"Industrie","fill":"#a42424"},
+        "US2.4":{"title":"Production d'énergie","fill":"#e04f3c"},
+        "US2.5":{"title":"Autres Industries","fill":"#ffcc00"},
+        "US3.1":{"title":"Commerce","fill":"#b18acc"},
+        "US3.23.1":{"title":"Finance, information, communication","fill":"#894465"},
+        "US3.23.2":{"title":"Finance et information/communication","fill":"#894465"},
+        "US3.4":{"title":"Loisirs, culturels…","fill":"#ffbee8"},
+        "US3.5":{"title":"Autres services","fill":"#cd6699"},
+        "US4.1.1":{"title":"Transport routier","fill":"#ff2c29"},
+        "US4.1.2":{"title":"Transport ferrovière","fill":"#6d071a"},
+        "US4.1.3":{"title":"Aérien","fill":"#bec9e8"},
+        "US4.1.4":{"title":"Fluvial et maritime","fill":"#297fed"},
+        "US4.1.5":{"title":"Autre","fill":"#343434"},
+        "US4.2":{"title":"Services logistiques et de stockage","fill":"#5a5e6b"},
+        "US4.3":{"title":"Réseaux d'utilité publique","fill":"#83a697"},
+        "US5.1.1":{"title":"Individuel","fill":"#fd6270"},
+        "US5.1.2":{"title":"Collectif","fill":"#fd6270"},
+        "US5.2.1":{"title":"Individuel","fill":"#fd6270"},
+        "US5.2.2":{"title":"Collectif","fill":"#fd6270"},
+        "US5.3":{"title":"Autre usage résidentiel","fill":"#fd6270"},
+        "US6.1":{"title":"Zones de transition (ou de construction)","fill":"#e9d6d2"},
+        "US6.2":{"title":"Zone abandonnée (friche)","fill":"#842e1b"},
+        "US6.3":{"title":"Sans usages","fill":"#a98c78"},
+        "US6.4":{"title":"Inconnu","fill":"#503737"}
+    };
+
+
+
+mviewer.customLayers.ocs56_usage.style0 = function (feature, resolution) {
+    var level = "";
+    var ref = "";
+    var val = feature.get("usage");
+    if (resolution > 76) {
+            level = "a";
+            ref = val.substring(0,3)+"%";
+    } else if (resolution > 38) {
+            level = "b";
+            if (val !== "US2.123") {
+                ref = val.substr(0,3)+"?"+val.substr(4,1)+"%";
+            } else {
+                ref = val;
+            }
+    }  else if (resolution > 2) {
+            level = "c";
+            ref = val;
+    }
+    var style;
+    if (mviewer.customLayers.ocs56_usage.classification_usage[level][ref]) {
+        feature.getProperties()["label"] = mviewer.customLayers.ocs56_usage.classification_usage[level][ref].title;
+        style = new ol.style.Style({
+            fill: new ol.style.Fill({
+              color: mviewer.customLayers.ocs56_usage.classification_usage[level][ref].fill
+            }),
+            stroke: new ol.style.Stroke({
+              color: mviewer.customLayers.ocs56_usage.classification_usage[level][ref].fill,
+              width: 0
+            })
+        });
+    } else {
+        console.log(ref, val);
+        style = new ol.style.Style();
+    }
+    return style;
+};
+
+
+mviewer.customLayers.ocs56_usage.layer = new ol.layer.VectorTile({
+      style:  mviewer.customLayers.ocs56_usage.style0,
+      minResolution: 2,
+      maxResolution: 306,
+      source: new ol.source.VectorTile({
+        tilePixelRatio: 1, // oversampling when > 1
+        tileGrid: ol.tilegrid.createXYZ({maxZoom: 20}),
+        format: new ol.format.MVT(),
+        url: 'https://ows.region-bretagne.fr/geoserver/gwc/service/tms/1.0.0/rb:ocs56_origine@EPSG%3A3857@pbf/{z}/{x}/{-y}.pbf'
+      })
+});
+mviewer.customLayers.ocs56_usage.handle = false;
+}
