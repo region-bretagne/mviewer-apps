b1fdbba0f9f3ca8112bcb8ad53d794a63a2f953f
spelhate <seb.pelhate@gmail.com>
2018-11-07 11:05:16 +0100

    Update typeahead

--- patrimoine/addons/recherche/html/inventaire.js
+++ patrimoine/addons/recherche/html/inventaire.js
@@ -12,8 +12,35 @@ mviewer.customControls.inventaire = (function() {
 
     var _collection = false;
 
+    var charMap = {
+        "à": "a",
+        "á": "a",
+        "â": "a",
+        "é": "e",
+        "è": "e",
+        "ê": "e",
+        "ë": "e",
+        "É": "e",
+        "ï": "i",
+        "î": "i",
+        "ô": "o",
+        "ö": "o",
+        "û": "u",
+        "ù": "u",
+        "ü": "u",
+        "ñ": "n"
+    };
+
+    var normalize = function (input) {
+        $.each(charMap, function (unnormalizedChar, normalizedChar) {
+            var regex = new RegExp(unnormalizedChar, 'gi');
+            input = input.replace(regex, normalizedChar);
+        });
+        return input;
+    };
+
     var _updateLayer = function() {
-        var values = $("#inventaire_search_queries").tagsinput('items') || [];
+        var values = $.map($("#inventaire_search_queries").tagsinput('items') || [], function (item) { return item.value; });
         mviewer.customLayers.inventaire.setFilter(values);
         mviewer.customLayers.inventaire.layer.getSource().getSource().clear(false);
         _lastValues = values.join(",");
@@ -28,19 +55,52 @@ mviewer.customControls.inventaire = (function() {
     };
 
     var _initForm = function () {
+
+        var queryTokenizer = function (q) {
+            var normalized = normalize(q);
+            return Bloodhound.tokenizers.whitespace(normalized);
+        };
+
+        var dico = new Bloodhound({
+            datumTokenizer: Bloodhound.tokenizers.obj.whitespace('nor'),
+            queryTokenizer: queryTokenizer,
+            local: _collection
+        });
+
+        dico.initialize();
+
         $("#inventaire_search_queries").tagsinput({
-            typeahead: {
-                source: _collection
-            },
+            itemValue: 'value',
+            itemText: 'nor',
+            typeaheadjs: [
+            {
+                name: 'dico',
+                source: dico.ttAdapter(),
+                items: 10,
+                displayKey: 'nor',
+                displayText: function (item){ return item.nor },
+                updater: function(item) {
+                     $("#inventaire_search_queries").tagsinput("add", item);
+                },
+                matcher: function (item) {
+                    return (item.nor.toLowerCase().indexOf(normalize(this.query.toLowerCase())) != -1)
+                }
+            }
+            ],
             freeInput: false,
             tagClass: 'label label-mv'
         });
+
         if (_defaultValue && !_lastValues) {
-            $("#inventaire_search_queries").tagsinput("add", _defaultValue);
+            _defaultValue.split(",").forEach(function (value) {
+                $("#inventaire_search_queries").tagsinput("add", {value: value, nor: value});
+            });
             _defaultValue = false;
             _updateLayer();
         } else if (_lastValues) {
-            $("#inventaire_search_queries").tagsinput("add", _lastValues);
+            _lastValues.split(",").forEach(function (value) {
+                $("#inventaire_search_queries").tagsinput("add", {value: value, nor: value});
+            });
             _defaultValue = false;
             _updateLayer();
         }
@@ -66,7 +126,9 @@ mviewer.customControls.inventaire = (function() {
             // mandatory - code executed when panel is opened
             if (!_collection) {
                 $.getJSON("apps/region/patrimoine/addons/recherche/collection.json", function(data){
-                    _collection = data;
+                    _collection = $.map(data, function(item){
+                        return {value: item, nor:normalize(item)};
+                    });
                     _initForm();
                 });
             } else {
