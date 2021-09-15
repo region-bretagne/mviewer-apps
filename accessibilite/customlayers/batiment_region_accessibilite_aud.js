410207e8c99b7a68bf3d9bc518b631dab2613ef9
lecault <loic.ecault@bretagne.bzh>
2021-09-15 16:54:17 +0200

    modif accessibilite

---
+++ accessibilite/customlayers/batiment_region_accessibilite_aud.js
@@ -0,0 +1,52 @@
+{
+mviewer.customLayers.batiment_region_accessibilite_aud = {};
+var batiment_region_accessibilite_aud = mviewer.customLayers.batiment_region_accessibilite_aud;
+
+batiment_region_accessibilite_aud.legend = { items: [
+    {
+        label: "Non accessible, non praticable",
+        geometry: "Polygon",
+        styles: [new ol.style.Style({
+            stroke: new ol.style.Stroke({ color: '#B0252E', width: 3 }),
+            fill: new ol.style.Fill({ color: '#C18A7E'})
+        })]
+    },
+    {
+        label: "Non accessible mais praticable",
+        geometry: "Polygon",
+        styles: [new ol.style.Style({
+            stroke: new ol.style.Stroke({ color: '#E3C213', width: 3 }),
+            fill: new ol.style.Fill({ color: '#EED969'})
+        })]
+    },    
+    {
+        label: "Accessible",
+        geometry: "Polygon",
+        styles: [new ol.style.Style({
+            stroke: new ol.style.Stroke({ color: '#9FB935', width: 3 }),
+            fill: new ol.style.Fill({ color: '#C1CA90'})
+        })]
+    }
+] };
+
+mviewer.customLayers.batiment_region_accessibilite_aud.layer = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            url: "https://ows.region-bretagne.fr/geoserver/rb/wfs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GETFEATURE&TYPENAME=batiment_region_accessibilite&outputFormat=application/json&srsName=EPSG:4326",
+            format: new ol.format.GeoJSON()
+        }),
+        style: function(feature, resolution) {
+            var stl;            
+            if (feature.get('classe_h_aud_txt') == 'Non accessible, non praticable'){
+                stl = batiment_region_accessibilite_aud.legend.items[0].styles;
+			}
+            else if (feature.get('classe_h_aud_txt') == 'Non accessible mais praticable'){
+                stl = batiment_region_accessibilite_aud.legend.items[1].styles;
+			}
+            else if (feature.get('classe_h_aud_txt') == 'Accessible'){
+                stl = batiment_region_accessibilite_aud.legend.items[2].styles;
+            }
+            return stl;
+        }
+});
+mviewer.customLayers.batiment_region_accessibilite_aud.handle = false;
+}
