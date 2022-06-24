3b1affe42c6d7d93b7a19d03e70d75bcee618cd5
lecault <loic.ecault@bretagne.bzh>
2022-06-24 11:22:59 +0200

    kartenn lycee

---
+++ lycee/customlayers/lycee_batiment.js
@@ -0,0 +1,26 @@
+{
+mviewer.customLayers.lycee_batiment = {};
+var lycee_batiment = mviewer.customLayers.lycee_batiment;
+
+lycee_batiment.legend = { items: [
+    {
+        label: "Bâtiment",
+        geometry: "Polygon",
+        styles: [new ol.style.Style({
+            stroke: new ol.style.Stroke({ color: '#641814', width: 3 }),
+            fill: new ol.style.Fill({ color: 'rgba(100, 25, 20, 0.3)'})
+        })]
+    }
+] };
+
+mviewer.customLayers.lycee_batiment.layer = new ol.layer.Vector({
+        source: new ol.source.Vector({
+            url: "https://ows.region-bretagne.fr/geoserver/rb/wfs?SERVICE=WFS&VERSION=1.0.0&REQUEST=GETFEATURE&TYPENAME=lycee_batiment&outputFormat=application/json&srsName=EPSG:4326",
+            format: new ol.format.GeoJSON()
+        }),
+        style: function(feature, resolution) {
+            return lycee_batiment.legend.items[0].styles;
+        }
+});
+mviewer.customLayers.lycee_batiment.handle = false;
+}
